e628e38345178414a8cc132778e57f3b818045b7
rmexia <remondo86@hotmail.fr>
2016-04-07 18:39:34 +0200

    Menu principal
    Apparition du menu principal dans une nouvelle scène.
    Actuellement seul le bouton Jouer marche. Le bouton Quitter n'est pas
    testable.

---
+++ League of Losers/Assets/Scripts/BackgroundRandomiser.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundRandomiser : MonoBehaviour {
+
+    public Sprite background1;
+    public Sprite background2;
+    
+
+	// Use this for initialization
+	void Start () {
+        switch (Random.Range(0, 2))
+        {
+            case 0: this.GetComponent<SpriteRenderer>().sprite = background1;break;
+            case 1: this.GetComponent<SpriteRenderer>().sprite = background2; break;
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ League of Losers/Assets/Scripts/GUIMainMenu.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GUIMainMenu : MonoBehaviour {
+
+    
+    public int buttonSizeX;
+    public int buttonSizeY;
+    private int unitX;
+    private int unitY;
+    // Use this for initialization
+    void Start ()
+    {
+        unitX = Screen.width / 64;
+        unitY = Screen.height / 64;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+
+    void OnGUI()
+    {
+        // Make the first button (Jouer)
+        if (GUI.Button(new Rect(unitX * 32 - buttonSizeX / 2, unitY * 22, buttonSizeX, buttonSizeY), "Jouer"))
+        {
+            SceneManager.LoadScene(1);
+        }
+        // Make the second button (Options)
+        if (GUI.Button(new Rect(unitX * 32 - buttonSizeX / 2, unitY * 24 + buttonSizeY, buttonSizeX, buttonSizeY), "Options"))
+        {
+            SceneManager.LoadScene(2);
+        }
+        // Make the third button (Credits)
+        if (GUI.Button(new Rect(unitX * 32 - buttonSizeX / 2, unitY * 26 + 2*buttonSizeY, buttonSizeX, buttonSizeY), "Credits"))
+        {
+            SceneManager.LoadScene(3);
+        }
+        // Make the fourth button (Quitter)
+        if (GUI.Button(new Rect(unitX * 32 - buttonSizeX / 2, unitY * 28 + 3 * buttonSizeY, buttonSizeX, buttonSizeY), "Quitter"))
+        {
+            Application.Quit();
+        }
+    }
+
+}
