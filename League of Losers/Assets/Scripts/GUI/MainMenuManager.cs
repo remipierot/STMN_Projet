58c82531a48da6536d13f6f2166a73403277a2bb
rmexia <remondo86@hotmail.fr>
2016-04-12 16:34:26 +0200

    Interface utilisateur avec le nouvel outil UI
    Interface utilisateur réalisée :
    - Menu principal (refait avec le nouvel outil UI de unity)
    - Menu d'options (non fonctionnel)
    - Créer et rejoindre une partie (non fonctionnel)
    - Crédits

---
+++ League of Losers/Assets/Scripts/GUI/MainMenuManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Button_Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
