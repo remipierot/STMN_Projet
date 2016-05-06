9557118ef002d3a7d529fea276a530e81e5fb172
jb <jonathan.bon@free.fr>
2016-05-06 23:23:39 +0200

    Visée perso (quelques bugs). !!grosse idiotie de notre part détectée: perso est déplacé par téléportation, résulte en collisions bugguées avec certaines plateformes (testez, vous comprendrez)

--- League of Losers/Assets/Scripts/PlayerAttackScript.cs
+++ League of Losers/Assets/Scripts/PlayerAttackScript.cs
@@ -4,15 +4,21 @@ using System.Collections.Generic;
 
 public class PlayerAttackScript : MonoBehaviour {
     
-    public float knockbackStrength = 4;         //Intensité du knockback
-    public BoxCollider2D detectArea;            //Zone de détection d'ennemis (corps à corps)
-    public Rigidbody2D m_Projectile;            //Projectile lancé par le personnage lors de l'attaque
-    public bool rangedAttack = true;            //Indique que le joueur utilise une attaque à distance. Autrement, indique une attaque corps à corps.
+    public float knockbackStrength = 4;             //Intensité du knockback
+    public BoxCollider2D detectArea;                //Zone de détection d'ennemis (corps à corps)
+    public Rigidbody2D m_Projectile;                //Projectile lancé par le personnage lors de l'attaque
+    public bool rangedAttack = true;                //Indique que le joueur utilise une attaque à distance. Autrement, indique une attaque corps à corps.
     
-    private Rigidbody2D m_Body;                 //Rigidbody2D de l'objet, utile pour le saut
-    private PhotonView m_PhotonView;    		//Objet lié au Network
+    private Rigidbody2D m_Body;                     //Rigidbody2D de l'objet, utile pour le saut
+    private PhotonView m_PhotonView;    		    //Objet lié au Network
+    private PlayerControllerScript m_ControlScript; //Script de contrôle du joueur
+    private Transform m_BodyBone;                   //Bone de la partie supérieure du perso, lors de la visée
     
-    private bool attacking = false;
+    private bool attacking = false;                 // vrai si on est en train d'attaquer / de viser
+    private bool wantAttack = false;                // vrai si le joueur est dans l'incapacité de viser (chute) mais veut viser
+    private float aimingAngle = 0;                  // angle visé lors de l'attaque
+    private float MsBeforeNextAngleUpdate = 150;     // temps avant la prochaine synchronisation d'angle de visée
+    private float AngleUpdateTimer = -1;            // timer de synchronisation d'angle de visée
     private Vector2 mouseStartPosition;
     
     private List<GameObject> playersInAttackRange = new List<GameObject>();
@@ -20,6 +26,23 @@ public class PlayerAttackScript : MonoBehaviour {
     void Awake() {
         m_Body = GetComponent<Rigidbody2D>();
         m_PhotonView = GetComponent<PhotonView>();
+        m_ControlScript = GetComponent<PlayerControllerScript>();
+        m_BodyBone = getChildByName(transform, "boneBODY");
+    }
+    
+    Transform getChildByName(Transform obj, string name)
+    {
+        if (obj.name == name)
+            return obj;
+        
+	    foreach (Transform child in obj)
+        {
+            Transform res = getChildByName(child, name);
+            if (res != null)
+                return res;
+        }
+        
+        return null;
     }
 
 	void Start () {
@@ -50,12 +73,22 @@ public class PlayerAttackScript : MonoBehaviour {
         }
         else
         {
-            if (Input.GetButtonDown("Attack"))
+            if (Input.GetButtonDown("Attack") && !attacking)
+                wantAttack = true;
+            if (m_ControlScript.CanAttack() && wantAttack)
             {
-                mouseStartPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-                // ajouter effets visuels de visée ici
+                wantAttack = false;
+                attacking = true;
+                if (m_ControlScript.GetCurrentFacing())
+                    mouseStartPosition = new Vector2(Input.mousePosition.x-10, Input.mousePosition.y);
+                else
+                    mouseStartPosition = new Vector2(Input.mousePosition.x+10, Input.mousePosition.y);
+                // change la position du perso
+                m_ControlScript.ChangeState(PlayerControllerScript.STATE_AIMING);
+                m_PhotonView.RPC("PhChangeState", PhotonTargets.Others, PlayerControllerScript.STATE_AIMING);
+                m_PhotonView.RPC("PhSetAttacking", PhotonTargets.Others, true);
             }
-            if (Input.GetButtonUp("Attack"))
+            if (Input.GetButtonUp("Attack") && attacking)
             {
                 Vector2 direction = (Vector2)(Input.mousePosition) - mouseStartPosition;
                 if (direction.magnitude < 40)
@@ -71,9 +104,85 @@ public class PlayerAttackScript : MonoBehaviour {
                 Rigidbody2D rb2d = projectileClone.GetComponent<Rigidbody2D>();
                 rb2d.GetComponent<Arrow>().setOwner(this.gameObject.GetComponent<PlayerControllerScript>().owner);
                 rb2d.velocity = direction * 15;
+                attacking = false;
+                AngleUpdateTimer = Time.realtimeSinceStartup * 1000;
+                m_ControlScript.ChangeState(PlayerControllerScript.STATE_IDLE);
+                m_PhotonView.RPC("PhChangeState", PhotonTargets.Others, PlayerControllerScript.STATE_IDLE);
+                m_PhotonView.RPC("PhSetAttacking", PhotonTargets.Others, false);
             }
         }
 	}
+    
+	void LateUpdate () {
+        if (!m_PhotonView.isMine)
+        {
+            SetAimDir();
+            return;
+        }
+        else if (attacking)
+        {
+            Vector2 direction = (Vector2)(Input.mousePosition) - mouseStartPosition;
+            direction.Normalize();
+            float angle = Vector2.Angle(new Vector2(1,0), direction);
+            if (m_ControlScript.GetCurrentFacing() && direction.x < 0)
+            {
+                m_ControlScript.ChangeDirection(false);
+                m_PhotonView.RPC("PhChangeDirection", PhotonTargets.Others, false);
+            }
+            if (!m_ControlScript.GetCurrentFacing() && direction.x > 0)
+            {
+                m_ControlScript.ChangeDirection(true);
+                m_PhotonView.RPC("PhChangeDirection", PhotonTargets.Others, true);
+            }
+            if (direction.y < 0)
+                angle *= -1;
+            aimingAngle = angle+90;
+            SetAimDir();
+            if ((Time.realtimeSinceStartup * 1000 - AngleUpdateTimer) > MsBeforeNextAngleUpdate)
+            {
+                AngleUpdateTimer = Time.realtimeSinceStartup * 1000;
+                m_PhotonView.RPC("PhSetAimDir", PhotonTargets.Others, aimingAngle);
+            }
+        }
+    }
+    
+    [PunRPC]
+    void PhSetAimDir(float angle)
+    {
+        aimingAngle = angle;
+        SetAimDir();
+    }
+    
+    [PunRPC]
+    void PhSetAttacking(bool attck)
+    {
+        attacking = attck;
+    }
+    
+    void SetAimDir()
+    {
+        Quaternion directionQuat = Quaternion.Euler(new Vector3(0, 0, aimingAngle));
+        if (aimingAngle > 180 || aimingAngle < 0)
+        {
+            // YOUPI, ON EST PAS DANS LA MERDE.
+            directionQuat *= (Quaternion.Euler(new Vector3(0, 180, 0)));
+            m_BodyBone.rotation = directionQuat;
+        }
+        else
+            m_BodyBone.rotation = directionQuat;
+    }
+    
+    public bool IsAiming()
+    {
+        return attacking;
+    }
+    
+    public void ExitAiming()
+    {
+        wantAttack = attacking;
+        attacking = false;
+        m_PhotonView.RPC("PhSetAttacking", PhotonTargets.Others, false);
+    }
     
     void OnTriggerEnter2D(Collider2D coll) {
         if (coll.gameObject.tag == "Player")
